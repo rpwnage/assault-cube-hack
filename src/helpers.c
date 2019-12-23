a5b6cca309623b2dce00d5a11f40e16bbc766439
rpwnage <tombston3wrq@gmail.com>
2019-12-23 20:38:55 +0100

    Working?

--- src/helpers.c
+++ src/helpers.c
@@ -23,7 +23,7 @@
 #include "offsets.h"
 char *EXECUTABLE_NAME = "assaultcube";
 int64_t aslr_slide = 0;
-
+size_t pageSize;
 
 void drawBitmapText(char *string, float x, float y, float z){
     char *c;
@@ -38,15 +38,15 @@ void set_mem_read_execute(void *addr, uint64_t size){
     return;
 }
 
+
 void set_mem_read_write(void *addr, uint64_t size){
     int ret;
-    if((ret = mprotect(addr, (size_t)size, (PROT_READ|PROT_WRITE|PROT_EXEC))) == 0){
-        printf("[PATCHER] Successfully set r-w-x [%p]\n", addr);
-    }else {
-        printf("[PATCHER] Failed while setting r-w-x [%p] (%d)\n", addr, ret);
-        printf("Address: %p\nSize: %llu\n", addr, size);
-        printf("ERROR: %s\n", strerror(errno));
-        exit(errno);
+    size_t pagesize = sysconf(_SC_PAGESIZE);
+    uintptr_t start = (uintptr_t) &addr;
+    uintptr_t end = start + size;
+    uintptr_t pagestart = start & -pagesize;
+    if ((ret = mprotect((void *) pagestart, end - pagestart, PROT_READ | PROT_WRITE | PROT_EXEC)) != 0){
+        exit(EXIT_FAILURE);
     }
     return;
 }
@@ -68,7 +68,7 @@ uint64_t calculate_address_with_aslr(uint64_t base, uint64_t offset_from_base){
 }
 
 uint64_t patch_jmp(void *addr, void *dst){
-    set_mem_read_write(addr, sizeof(uint64_t));
+ //   set_mem_read_write(addr, sizeof(uint64_t));
     uint64_t orig_bytes = *(uint64_t *)addr;
     printf("[JMP] original bytes: %08llx\n", orig_bytes);
     uint32_t jmp_offset = (uint32_t)((char*)dst - (char*)addr - 5);
@@ -77,7 +77,7 @@ uint64_t patch_jmp(void *addr, void *dst){
     uint64_t bytes = 0xE900000000000000LL;
     bytes |= ((uint64_t)jmp_offset & 0xFFFFFFFF) << 24;
     bytes = OSSwapInt64(bytes);
-    *(uint64_t *)addr = bytes;
+  //  *(uint64_t *)addr = bytes;
     printf("[JMP] offset: %08x\n", jmp_offset);
     printf("[PATCHER] replaced bytes: %08llx\n", bytes);
     
