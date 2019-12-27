6fe4e4553321589b150ef629a09c83b304faac43
rpwnage <tombston3wrq@gmail.com>
2019-12-27 14:25:43 +0100

    Function hooks (big message)

--- src/GameInternal/FunctionPatches.c
+++ src/GameInternal/FunctionPatches.c
@@ -4,8 +4,9 @@ uint64_t example_original;
 void patch_functions(){
     printf("[PATCHER] Patching test function...\n");
     // Initialize the hooks
-    sendChatMessage = (void*)(AC_BASE + 0x4B720);
+    sendChatMessage = (void*)(AC_BASE + 0x4B720);   // sendChatMessage("orksec rocks", 0x241);
+    sendBigMessage  = (void*)(AC_BASE + 0x91160);   // sendBigMessage("rpwnage rocks", 0x241);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        sendChatMessage("[PATCHER] Payload injected successfully.", 0x241);
+        sendBigMessage("rpwnage rocks", 0x241);
     });
 }
--- src/GameInternal/FunctionPatches.h
+++ src/GameInternal/FunctionPatches.h
@@ -3,6 +3,6 @@
 
 // Hooked functions
 void (*sendChatMessage)(char *one, long three);
-
+void (*sendBigMessage)(char *one, long three);
 // Function to initialize all Hooks
 void patch_functions();
